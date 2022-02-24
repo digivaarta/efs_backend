2f91127b25dc9f86300e313fcd071e2ab00d3edd
Sourav Gola <pythonist@Souravs-MBP.Dlink>
2022-02-24 22:01:43 +0530

    settings change

--- src/src/hosts.py
+++ src/src/hosts.py
@@ -3,7 +3,7 @@ from django_hosts import patterns, host
 host_patterns = patterns(
     '',
     #host(r'www', 'urls.master_urls', name='www'),
-    host(r'efs', 'urls.master_urls', name='efs'),
+    host(r'efs', 'urls.master_urls', name='admin'),
     #host(r'efs', 'urls.api_urls', name='api'),
     # host(r'docs','urls.docs_urls',name="docs"),
     # host(r'performance','src.performance_urls',name="performance"),
