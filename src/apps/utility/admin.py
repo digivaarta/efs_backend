435afc521bf021280f754b9ab58090898f3a674d
Sourav Gola <pythonist@Souravs-MBP.Dlink>
2022-03-08 17:21:28 +0530

    a

--- src/apps/utility/admin.py
+++ src/apps/utility/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-from utility.models import MetaContent
+from utility.models import MetaContent,MetaContentCategory
 # Register your models here.
+
+@admin.register(MetaContentCategory)
+class MetaContentCategoryAdmin(admin.ModelAdmin):
+    exclude = ("user",)
+    readonly_fields = ("slug",)
+    list_display = ("title","slug",)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+        return super().save_model(request, obj, form, change)
+
 
 @admin.register(MetaContent)
 class MetaContentAdmin(admin.ModelAdmin):
     exclude = ("user",)
+    readonly_fields = ("slug",)
     list_display = ("title","slug","creation_date","last_update","is_active",)
 
 
