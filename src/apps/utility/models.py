fac4c97d9ed37399a920627c6ff425c8a9e6e502
Sourav Gola <pythonist@Souravs-MBP.Dlink>
2022-03-08 17:19:12 +0530

    a

--- src/apps/utility/models.py
+++ src/apps/utility/models.py
@@ -5,8 +5,24 @@ from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 
 
+class MetaContentCategory(models.Model):
+
+    user = models.ForeignKey("account.User",on_delete=models.CASCADE,related_name="mcc_user")
+    title = models.CharField(max_length=400,unique=True)
+    slug = models.SlugField(max_length=600,blank=True,null=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super(MetaContentCategory, self).save(*args, **kwargs)
+    
+
 class MetaContent(models.Model):
     user = models.ForeignKey("account.User",related_name="user_mc",on_delete=models.CASCADE)
+    meta_category = models.ForeignKey("utility.MetaContentCategory",related_name="user_mcc",on_delete=models.CASCADE)
     title = models.CharField(max_length=400,unique=True)
     slug = models.SlugField(max_length=600,blank=True,null=True)
     content = RichTextUploadingField(blank=True,null=True)
